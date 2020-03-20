763bfa09eca7da993ab5a6c0a78899de61d4f821
jyp334 <jiyupeng@aragoncs.com>
2020-03-20 15:18:39 +0800

    update open notification page method

--- android/src/main/java/com/vanethos/notification_permissions/NotificationPermissionsPlugin.java
+++ android/src/main/java/com/vanethos/notification_permissions/NotificationPermissionsPlugin.java
@@ -35,12 +35,33 @@ public class NotificationPermissionsPlugin implements MethodChannel.MethodCallHa
     } else if ("requestNotificationPermissions".equalsIgnoreCase(call.method)) {
       if (PERMISSION_DENIED.equalsIgnoreCase(getNotificationPermissionStatus())) {
         if (context instanceof Activity) {
-          final Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+          try {
+            // 根据isOpened结果，判断是否需要提醒用户跳转AppInfo页面，去打开App通知权限
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+            //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
+            intent.putExtra(EXTRA_APP_PACKAGE, context.getPackageName());
+            intent.putExtra(EXTRA_CHANNEL_ID, context.getApplicationInfo().uid);
 
-          final Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-          intent.setData(uri);
-
-          context.startActivity(intent);
+            // 小米6 -MIUI9.6-8.0.0系统，是个特例，通知设置界面只能控制"允许使用通知圆点"——然而这个玩意并没有卵用，我想对雷布斯说：I'm not ok!!!
+            //  if ("MI 6".equals(Build.MODEL)) {
+            //      intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            //      Uri uri = Uri.fromParts("package", getPackageName(), null);
+            //      intent.setData(uri);
+            //      // intent.setAction("com.android.settings/.SubSettings");
+            //  }
+            context.startActivity(intent);
+          } catch (Exception e) {
+            e.printStackTrace();
+            // 出现异常则跳转到应用设置界面：锤子坚果3——OC105 API25
+            Intent intent = new Intent();
+            //下面这种方案是直接跳转到当前应用的设置界面。
+            //https://blog.csdn.net/ysy950803/article/details/71910806
+            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+            intent.setData(uri);
+            context.startActivity(intent);
+          }
 
           result.success(null);
         } else {
